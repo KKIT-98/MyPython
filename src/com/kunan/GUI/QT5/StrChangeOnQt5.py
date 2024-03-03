35337076eb58d1270a63b9aedd284e9baad3068a
KK_IT <yangkunan98@aliyun.com>
2024-03-03 12:21:44 +0800

    文本标签、文本框、转换复制按钮

--- src/com/kunan/GUI/QT5/StrChangeOnQt5.py
+++ src/com/kunan/GUI/QT5/StrChangeOnQt5.py
@@ -2,9 +2,6 @@ import re
 import sys
 from PyQt5.Qt import *
 
-import ctypes
-
-
 class StrChangeOnQt5(QWidget):
     def __init__(self):
         super().__init__()
@@ -20,7 +17,72 @@ class StrChangeOnQt5(QWidget):
         # 为控件设置文本 待转换
         self.labelTex1.setText("待转换文本")
         # 字体样式设置
-        self.labelTex1.setFont(QFont)
+        self.labelTex1.setFont(QFont("黑体", 18, QFont.Bold))
+        # 移动控件的位置
+        self.labelTex1.move(50, 10)
+
+        # 创建文本标签控件: 转换后文本
+        self.labelTex2 = QLabel(self)
+        # 为控件设置文本 待转换
+        self.labelTex2.setText("转换后文本")
+        # 字体样式设置
+        self.labelTex2.setFont(QFont("黑体", 18, QFont.Bold))
+        # 移动控件的位置
+        self.labelTex2.move(350, 10)
+
+        """多行文本框 待转换文本"""
+        self.TextEdit1 = QTextEdit(self)
+        # 设置字体颜色
+        # self.TextEdit1.setTextColor(QColor(0, 0, 225))
+        # 设置字体背景颜色
+        # self.TextEdit1.setTextBackgroundColor(QColor(255, 0, 255))
+        # 设置字体样式
+        self.TextEdit1.setFont(QFont("TimesNewRoman", 14, QFont.Thin))
+        # 水平滚动条
+        # self.TextEdit1.setHorizontalScrollBar()
+        # 移动控件的位置
+        self.TextEdit1.move(20, 50)
+        # 设置控件大小
+        self.TextEdit1.resize(200, 600)
+
+        """多行文本框 转换完文本"""
+        self.TextEdit2 = QTextEdit(self)
+        # 设置字体颜色
+        # self.TextEdit2.setTextColor(QColor(0, 0, 225))
+        # 设置字体背景颜色
+        # self.TextEdit2.setTextBackgroundColor(QColor(255, 0, 255))
+        # 设置字体样式
+        self.TextEdit2.setFont(QFont("TimesNewRoman", 14, QFont.Thin))
+        # 水平滚动条
+        # self.TextEdit1.setHorizontalScrollBar()
+        # 移动控件的位置
+        self.TextEdit2.move(300, 50)
+        # 设置控件大小
+        self.TextEdit2.resize(200, 600)
+
+        """转换按钮"""
+        self.ChangeBtn = QPushButton(self)
+        # 设置按钮文本
+        self.ChangeBtn.setText("转 换")
+        # 设置按钮位置
+        self.ChangeBtn.move(20, 660)
+        # 设置按钮大小
+        self.ChangeBtn.resize(200, 50)
+        # 设置按钮字体样式
+        self.ChangeBtn.setFont(QFont("宋体", 14, QFont.Bold))
+
+        """复制按钮"""
+        self.CopyBtn = QPushButton(self)
+        # 设置按钮文本
+        self.CopyBtn.setText("复 制")
+        # 设置按钮位置
+        self.CopyBtn.move(300, 660)
+        # 设置按钮大小
+        self.CopyBtn.resize(200, 50)
+        # 设置按钮字体样式
+        self.CopyBtn.setFont(QFont("宋体", 14, QFont.Bold))
+
+
 
 if __name__ == '__main__':
     # 创建一个应用程序对象
